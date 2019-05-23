fd65582984fe695a9fbd9de77be14e8e1c3f4dff
kauparno <50718450+kauparno@users.noreply.github.com>
2019-05-23 16:38:38 -0400

    Update StudentTester.java

--- src/studenttemplateclass/StudentTester.java
+++ src/studenttemplateclass/StudentTester.java
@@ -2,7 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- May23rd,2019 Noor*/
+ May23rd,2019 Noor sheridan college*/
 package studenttemplateclass;
 
 
